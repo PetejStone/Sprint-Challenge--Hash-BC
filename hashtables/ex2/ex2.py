de352839d1e2a3b6898abf7aa2da3b747d32e557
PetejStone <petejstoney@gmail.com>
2019-11-01 16:57:55 -0700

    mvp complete?

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -16,28 +16,24 @@ def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
     
-    start = None 
-    route = []
-    source = None 
+    start = None  # starting destination
+    route = [] #route list to be returned
+    source = None  # source of the next destination
     #next source === current destination
-    for i in range(length):
-        hash_table_insert(hashtable, tickets[i].source, tickets[i].destination)
-        if tickets[i].source == "NONE":
-            start = tickets[i]
-            source = start.destination
-            route.append(source)
+    for i in range(length): #for every instance of ticket
+        hash_table_insert(hashtable, tickets[i].source, tickets[i].destination) #add to hash table with source as key, and destination as value
+        if tickets[i].source == "NONE": # if the source is NONE, this is the first tripe
+            start = tickets[i] #set this ticket to the starting point
+            source = start.destination #set the source of this ticket (destination)
+            route.append(source) #append the destination of the starting point to the route
 
-    new_dest = hash_table_retrieve(hashtable, source)
-    print(source)
-    print(new_dest)
-    # print(new_dest)
+   
     
-    while len(route) < length and source:
-        next_dest = hash_table_retrieve(hashtable, source)
-        print('hello')
-        route.append(next_dest)
-        source = next_dest
-        print(route)
+    while len(route) < length and source: #while the length of route is < length and source is not none
+        next_dest = hash_table_retrieve(hashtable, source) # access next dest by using source, which is the starting point for new destination
+        route.append(next_dest) #append new destination
+        source = next_dest #set source to next destination
+    
     
         
 
