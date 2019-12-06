9ffa2ea15697f1383afec353c0c0e73ed74106c7
PetejStone <petejstoney@gmail.com>
2019-12-06 09:18:04 -0800

    added comments

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -49,7 +49,7 @@ def valid_proof(last_hash, proof):
     guess = f'{last_hash}{proof}'.encode()
     guess_hash = hashlib.sha256(guess).hexdigest()
     # return True or False
-    return last_hash[-6:] == guess_hash[:6]
+    return last_hash[:-6] == guess_hash[6:]
 
 if __name__ == '__main__':
     # What node are we interacting with?
--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -11,38 +11,23 @@ def get_indices_of_item_weights(weights, length, limit):
      
     
     for i in range(len(weights)):
-        # print(weights[i])
-        #print(limit)
-        hash_table_insert(ht, weights[i], i)
+        #create a hash table of all weights
+        hash_table_insert(ht, weights[i], i) #inserting weight into hashtable with it's key and value
     
     for i in range(len(weights)):
-        new_value = limit - weights[i]
-        checker = None
-        # print(new_value)
-        # {
-        #     4: 0,
-        #     6: 1,
-        #     15: 3
-
-        # }
-        # linked_pair = hash_table_retrieve(ht, 4)
-        # print(f'LINKED PAIR {linked_pair}')
-        if hash_table_retrieve(ht, new_value):
-            checker = i
-            new_value = hash_table_retrieve(ht, new_value)
-            # print(checker)
-            # print(hash_table_retrieve(ht, new_value))
-            if checker > new_value:
+        new_value = limit - weights[i] #subtracting limit from the given weight for each weight in weights
+        checker = None #checker default to None
+       
+        if hash_table_retrieve(ht, new_value): #if any of the weights === the difference, then these two add up to the limit
+            checker = i  #set checker equal to the current weight
+            new_value = hash_table_retrieve(ht, new_value) #new value is now equal to that value in the hashtable
+        
+            #put greater item first
+            if checker > new_value: #if weight is greater
                 return (checker, new_value)
             else:
                 return (new_value, checker)
-        # print(checker)
-        # print(new_value)
-        
-        # if checker > new_value:
-        #     return (checker,new_value)
-        # else:
-        #     return (new_value, checker)
+    
     return None
 
 
